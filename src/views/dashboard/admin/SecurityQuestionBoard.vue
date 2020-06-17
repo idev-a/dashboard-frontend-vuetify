4290de5e41a240adc0d8c3e4b494195e05e42eb0
idev-a <ideveloper003@protonmail.com>
2020-06-17 05:24:00 -0700

    implement Q&A board

--- src/views/dashboard/admin/SecurityQuestionBoard.vue
+++ src/views/dashboard/admin/SecurityQuestionBoard.vue
@@ -433,11 +433,12 @@
 		      		{
 			            text: 'Question',
 			            value: 'question',
+			            width: 350
 		          	},
 		          	{
 			            text: 'Answer',
 			            value: 'answer',
-			            width: '200'
+			            width: 400
 		          	},
 		          	{ text: 'Actions', value: 'action', sortable: false }
 		      	],
