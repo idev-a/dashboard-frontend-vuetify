c9d7f5b998b7830c2b787196df15d0c7f27cbcb2
idev-a <ideveloper003@protonmail.com>
2020-07-31 09:10:28 -0700

    revise modal

--- src/views/dashboard/admin/O365Board.vue
+++ src/views/dashboard/admin/O365Board.vue
@@ -268,7 +268,7 @@
             method: 'POST'
           })
           if (res.status == 200) {
-            window.open(res.data.auth_url, '_blank', "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=800,height=600")
+            window.open(res.data.auth_url, '_blank', "toolbar=yes,replace=false,clearcache=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=800,height=600,noreferrer")
           }
         } catch (e) {
           console.log(e.response)
